09ac30355480cb7e286d5e46bc7ef4fbce349036
mattphillips <mattphillips1016@gmail.com>
2015-10-25 23:32:42 +0000

    Added routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,18 @@
 var rpsApp = angular.module('rpsApp', [
+        'ngRoute',
         'GameLogicService',
         'HandTypeService',
         'MathService',
         'HandGeneratorService'
     ]
 );
+
+
+rpsApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'templates/home.tpl.html'
+        });
+    }
+]);
