4b4196be6bc1bd11b5bd6352944da000222a1020
mattphillips <mattphillips1016@gmail.com>
2015-10-25 21:25:46 +0000

    Created a math service to perform missing standard library functions

---
+++ app/js/services/Math.js
@@ -0,0 +1,14 @@
+// This is a nasty service to provide an interface to Math functions required that aren't standard in JS
+var MathService = angular.module('MathService', []);
+// named to avoid collisions with keyword Math
+MathService.service('Maths', function(){
+
+    this.getRandomNumber = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    // js Modulus does not play nice with negatives
+    this.mod = function(m, n) {
+         return ((m % n) + n) % n;
+    }
+});
