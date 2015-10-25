103218589575c910512e8dda1b3a8b35e1a3df00
mattphillips <mattphillips1016@gmail.com>
2015-10-25 21:40:18 +0000

    Added tests to cover each case

--- test/unit/services/GameLogicSpec.js
+++ test/unit/services/GameLogicSpec.js
@@ -1,5 +1,10 @@
 describe('Tests for GameLogicService', function() {
 
+    const ROCK     = new Hand("rock", 1),
+          PAPER    = new Hand("paper", 2),
+          SCISSORS = new Hand("scissors", 3),
+          TIE      = "TIE";
+
     // load modules
     beforeEach(module('rpsApp'));
 
@@ -10,10 +15,73 @@ describe('Tests for GameLogicService', function() {
 
     it('should return player one as winner with rock beating scissors', inject(function(GameLogic) {
 
-        var playerOne = new Player("winner", new Hand("rock", 1));
-        var playerTwo = new Player("loser", new Hand("scissors", 3));
+        var playerOne = new Player("winner", ROCK);
+        var playerTwo = new Player("loser", SCISSORS);
 
         expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(playerOne);
     }));
 
+    it('should return player one as winner with paper beating rock', inject(function(GameLogic) {
+
+        var playerOne = new Player("winner", PAPER);
+        var playerTwo = new Player("loser", ROCK);
+
+        expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(playerOne);
+    }));
+
+    it('should return player one as winner with scissors beating paper', inject(function(GameLogic) {
+
+        var playerOne = new Player("winner", SCISSORS);
+        var playerTwo = new Player("loser", PAPER);
+
+        expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(playerOne);
+    }));
+
+    it('should return player two as winner with rock beating scissors', inject(function(GameLogic) {
+
+        var playerOne = new Player("loser", SCISSORS);
+        var playerTwo = new Player("winner", ROCK);
+
+        expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(playerTwo);
+    }));
+
+    it('should return player two as winner with paper beating rock', inject(function(GameLogic) {
+
+        var playerOne = new Player("loser", ROCK);
+        var playerTwo = new Player("winner", PAPER);
+
+        expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(playerTwo);
+    }));
+
+    it('should return player two as winner with scissors beating paper', inject(function(GameLogic) {
+
+        var playerOne = new Player("loser", PAPER);
+        var playerTwo = new Player("winner", SCISSORS);
+
+        expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(playerTwo);
+    }));
+
+    it('should return tie when both players have the same rock as their hand', inject(function(GameLogic) {
+
+        var playerOne = new Player("p1", ROCK);
+        var playerTwo = new Player("p2", ROCK);
+
+        expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(TIE);
+    }));
+
+    it('should return tie when both players have the same paper as their hand', inject(function(GameLogic) {
+
+        var playerOne = new Player("p1", PAPER);
+        var playerTwo = new Player("p2", PAPER);
+
+        expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(TIE);
+    }));
+
+    it('should return tie when both players have the same scissors as their hand', inject(function(GameLogic) {
+
+         var playerOne = new Player("p1", SCISSORS);
+         var playerTwo = new Player("p2", SCISSORS);
+
+        expect(GameLogic.determineWinningPlayer(playerOne, playerTwo)).toEqual(TIE);
+    }));
 });
