d4f9e6c658523d5a3881f1afc1d0db3ccc8b359d
mattphillips <mattphillips1016@gmail.com>
2015-10-25 19:32:52 +0000

    Added initial karma config to run tests

---
+++ test/karma.conf.js
@@ -0,0 +1,45 @@
+module.exports = function(config){
+    config.set({
+
+        basePath : '../',
+
+        files : [
+            'app/bower_components/jquery/dist/jquery.js',
+            'app/bower_components/angular/angular.js',
+            'app/js/**/*.js',
+            'test/unit/**/*.js'
+        ],
+
+        autoWatch : true,
+
+        frameworks: ['jasmine'],
+
+        browsers : ['PhantomJS'],
+
+        plugins : [
+            'karma-phantomjs-launcher',
+            'karma-jasmine',
+            'karma-coverage'
+        ],
+
+        junitReporter : {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        },
+
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'test/unit/**/*.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'test/coverage/'
+    }
+    });
+};
